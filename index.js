b028e0bc71abb5390ec4bbce9cbc5ce10e040f4c
Bartosz T <btrafidlo@yahoo.com>
2022-01-21 00:16:33 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"];
+
+ beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+ 
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+function appendCat(name) {
+    var cat= [...cats,name];
+    return cat;
+}
+function prependCat(name) {
+    var catt=[name, ...cats];
+    return catt;
+}
+function removeLastCat() {
+    var caat=cats.slice(0,cats.length - 1);
+    return caat
+}
+function removeFirstCat() {
+    var caatt=cats.slice(1);
+    return caatt
+}
